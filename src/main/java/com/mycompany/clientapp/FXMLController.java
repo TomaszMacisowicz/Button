7599403765955c248239661448286c9913e6ea48
Tomasz Macisowicz <tmacisowicz@gmail.com>
2017-05-04 15:06:50 +0200

    change display text

--- src/main/java/com/mycompany/clientapp/FXMLController.java
+++ src/main/java/com/mycompany/clientapp/FXMLController.java
@@ -8,18 +8,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        label.setText("So let's go!");
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
